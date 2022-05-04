886009b99fc77166c2dbaea63c00dce1110b447c
1000marin <leexjin@gmail.com>
2022-05-04 11:16:30 +0900

    __init__

---
+++ 9_2.py
@@ -0,0 +1,15 @@
+# __init__
+
+class Unit:
+    def __init__(self, name, hp, damage) -> None:
+        self.name = name
+        self.hp = hp
+        self.damege = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damege))
+
+marine1 = Unit("마린", 40, 5)
+marine2 = Unit("마린", 40, 5)
+tank = Unit("탱크", 150, 35)
+
+#tank2 = Unit("탱크2")
