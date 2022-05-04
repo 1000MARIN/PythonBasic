c0ec5253eca9cb5b9a3095362f1ff097c0e3ecba
1000marin <leexjin@gmail.com>
2022-05-04 14:48:05 +0900

    __init__

--- 9_2.py
+++ 9_2.py
@@ -1,7 +1,7 @@
 # __init__
 
 class Unit:
-    def __init__(self, name, hp, damage) -> None:
+    def __init__(self, name, hp, damage):
         self.name = name
         self.hp = hp
         self.damege = damage
