020ffa399706fd5018af37f53953c6f78f04825b
1000marin <leexjin@gmail.com>
2022-05-06 15:54:01 +0900

    __all__

---
+++ 11_3.py
@@ -0,0 +1,17 @@
+# __all__
+# # import travel.thailand
+# trip_to = travel.thailand.ThailandPackge()
+# trip_to.detail()
+
+# from travel.thailand import ThailandPackge
+# trip_to = ThailandPackge()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+from travel import *
+# trip_to = vietnam.VietnamPackage()
+trip_to = thailand.ThailandPackge()
+trip_to.detail()
