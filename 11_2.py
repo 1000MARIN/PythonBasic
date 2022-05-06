97d49bb7d15e1d76e1d3c75443cb480e66c1c3b9
1000marin <leexjin@gmail.com>
2022-05-06 15:48:18 +0900

    패키지

---
+++ 11_2.py
@@ -0,0 +1,12 @@
+# 패키지
+# # import travel.thailand
+# trip_to = travel.thailand.ThailandPackge()
+# trip_to.detail()
+
+# from travel.thailand import ThailandPackge
+# trip_to = ThailandPackge()
+# trip_to.detail()
+
+from travel import vietnam
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
