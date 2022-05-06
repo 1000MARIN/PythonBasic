5aa7bcea95d2c99da30e9ac264ec66834381f1dc
1000marin <leexjin@gmail.com>
2022-05-06 16:07:45 +0900

    모듈 직접 실행

---
+++ 11_4.py
@@ -0,0 +1,18 @@
+# 모듈 직접 실행
+
+# # import travel.thailand
+# trip_to = travel.thailand.ThailandPackge()
+# trip_to.detail()
+
+# from travel.thailand import ThailandPackge
+# trip_to = ThailandPackge()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+from travel import *
+# trip_to = vietnam.VietnamPackage()
+trip_to = thailand.ThailandPackge()
+trip_to.detail()
